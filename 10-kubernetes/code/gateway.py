d3d2271441f043868f196130365038a7bb5dd921
KonuTech <borowiec.k@gmail.com>
2023-12-03 21:02:32 +0100

    Changes realted to Module 10

--- 10-kubernetes/code/gateway.py
+++ 10-kubernetes/code/gateway.py
@@ -17,39 +17,40 @@ from flask import jsonify
 
 from proto import np_to_protobuf
 
-host = os.getenv('TF_SERVING_HOST', 'localhost:8500')
+host = os.getenv("TF_SERVING_HOST", "localhost:8500")
 
 channel = grpc.insecure_channel(host)
 stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
-preprocessor = create_preprocessor('xception', target_size=(299, 299))
+preprocessor = create_preprocessor("xception", target_size=(299, 299))
 
 
 def prepare_request(X):
     pb_request = predict_pb2.PredictRequest()
 
-    pb_request.model_spec.name = 'clothing-model'
-    pb_request.model_spec.signature_name = 'serving_default'
+    pb_request.model_spec.name = "clothing-model"
+    pb_request.model_spec.signature_name = "serving_default"
 
-    pb_request.inputs['input_8'].CopyFrom(np_to_protobuf(X))
+    pb_request.inputs["input_8"].CopyFrom(np_to_protobuf(X))
     return pb_request
 
 
 classes = [
-    'dress',
-    'hat',
-    'longsleeve',
-    'outwear',
-    'pants',
-    'shirt',
-    'shoes',
-    'shorts',
-    'skirt',
-    't-shirt'
+    "dress",
+    "hat",
+    "longsleeve",
+    "outwear",
+    "pants",
+    "shirt",
+    "shoes",
+    "shorts",
+    "skirt",
+    "t-shirt",
 ]
 
+
 def prepare_response(pb_response):
-    preds = pb_response.outputs['dense_7'].float_val
+    preds = pb_response.outputs["dense_7"].float_val
     return dict(zip(classes, preds))
 
 
@@ -61,19 +62,19 @@ def predict(url):
     return response
 
 
-app = Flask('gateway')
+app = Flask("gateway")
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict_endpoint():
     data = request.get_json()
-    url = data['url']
+    url = data["url"]
     result = predict(url)
     return jsonify(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # url = 'http://bit.ly/mlbookcamp-pants'
     # response = predict(url)
     # print(response)
-    app.run(debug=True, host='0.0.0.0', port=9696)
+    app.run(debug=True, host="0.0.0.0", port=9696)
